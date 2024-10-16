e6155d94cd88ee8bedcf67d0201fc00d2f28fa00
Akinola Soyinka <44506742+AkinOlaSoyInka@users.noreply.github.com>
2024-10-16 19:32:55 +0100

    Created route tree and start page

---
+++ src/app/start/page.tsx
@@ -0,0 +1,9 @@
+export default function Page(){
+    return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <h1 className="header">TacticWizz</h1>
+        <p className="intro"> Want to learn more about the beautiful game?</p>
+        <button className="landingButton"> Continue </button>
+    </div>
+)
+}
